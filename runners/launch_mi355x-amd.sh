1031ac957ec607bb586bceda5179ab2145e6836f
Cam Quilici <cjquilici@gmail.com>
2025-11-14 11:50:53 -0600

    fix mi355x docker NUM_PROMPTS

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -19,13 +19,23 @@ PORT=8888
 
 server_name="bmk-server"
 
+if [[ "$MODEL" == "amd/DeepSeek-R1-0528-MXFP4-Preview" || "$MODEL" == "deepseek-ai/DeepSeek-R1-0528" ]]; then
+  if [[ "$OSL" == "8192" ]]; then
+    export NUM_PROMPTS=$(( CONC * 20 ))
+  else
+    export NUM_PROMPTS=$(( CONC * 50 ))
+  fi
+else
+  export NUM_PROMPTS=$(( CONC * 10 ))
+fi
+
 set -x
 docker run --rm --ipc=host --shm-size=16g --network=host --name=$server_name \
 --privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
--e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
+-e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT -e NUM_PROMPTS \
 -e ISL -e OSL -e PYTHONPYCACHEPREFIX=/tmp/pycache/ -e RANDOM_RANGE_RATIO -e RESULT_FILENAME  \
 --entrypoint=/bin/bash \
 $IMAGE \
