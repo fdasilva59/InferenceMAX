83901e7427bbd4c6cc46de9e26eb80dbdcff9fd0
Oseltamivir <bryansg2013@gmail.com>
2025-11-15 14:02:45 -0600

    evals h200-nb

--- benchmarks/gptoss_fp4_h200_slurm.sh
+++ benchmarks/gptoss_fp4_h200_slurm.sh
@@ -67,3 +67,7 @@ run_benchmark_serving \
     --max-concurrency "$CONC" \
     --result-filename "$RESULT_FILENAME" \
     --result-dir /workspace/
+
+# After throughput, run evaluation (defaults to GSM8K)
+run_lm_eval --port "$PORT"
+append_lm_eval_summary
